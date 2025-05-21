83e8046bb411c134d5cf971164582bb734cf0922
erinlivingston <elivingston@gradcenter.cuny.edu>
2025-05-21 17:39:54 -0400

    Update colorExtraction.js

--- colorExtraction.js
+++ colorExtraction.js
@@ -16,52 +16,52 @@ const imagePathsByYear = {
         "assets/2019-Tickets/12.30.19.pm1.PNG"],
 //2020 image file paths
     "2020": [
-        "/assets/2020-Tickets/01.02.20.am7.PNG","/assets/2020-Tickets/01.02.20.pm5.PNG","/assets/2020-Tickets/01.03.20.am7.PNG","/assets/2020-Tickets/01.03.20.pm4.PNG","/assets/2020-Tickets/01.06.20.am7.PNG",
-            "/assets/2020-Tickets/01.06.20.pm5.PNG","/assets/2020-Tickets/01.08.20.am7.PNG","/assets/2020-Tickets/01.08.20.pm5.PNG","/assets/2020-Tickets/01.10.20.am7.PNG","/assets/2020-Tickets/01.10.20.pm5.PNG","/assets/2020-Tickets/01.13.20.am7.PNG",
-            "/assets/2020-Tickets/01.13.20.pm5.PNG","/assets/2020-Tickets/01.15.20.am7.PNG","/assets/2020-Tickets/01.15.20.pm5.PNG","/assets/2020-Tickets/01.16.20.am8.PNG","/assets/2020-Tickets/01.16.20.pm4.PNG","/assets/2020-Tickets/01.21.20.am7.PNG",
-            "/assets/2020-Tickets/01.21.20.pm5.PNG","/assets/2020-Tickets/01.22.20.am7.PNG","/assets/2020-Tickets/01.22.20.pm5.PNG","/assets/2020-Tickets/01.28.20.am7.PNG","/assets/2020-Tickets/01.28.20.pm5.PNG","/assets/2020-Tickets/01.29.20.am7.PNG",
-            "/assets/2020-Tickets/01.29.20.pm5.PNG","/assets/2020-Tickets/01.30.20.am7.PNG","/assets/2020-Tickets/01.30.20.pm5.PNG","/assets/2020-Tickets/02.01.20.am7.PNG","/assets/2020-Tickets/02.03.20.pm5.PNG","/assets/2020-Tickets/02.04.20.am7.PNG",
-            "/assets/2020-Tickets/02.05.20.am7.PNG","/assets/2020-Tickets/02.05.20.pm8.PNG","/assets/2020-Tickets/02.06.20.am7.PNG","/assets/2020-Tickets/02.06.20.pm5.PNG","/assets/2020-Tickets/02.10.20.am7.PNG","/assets/2020-Tickets/02.10.20.pm5.PNG",
-            "/assets/2020-Tickets/02.11.20.am7.PNG","/assets/2020-Tickets/02.11.20.pm5.PNG","/assets/2020-Tickets/02.13.20.am7.PNG","/assets/2020-Tickets/02.13.20.pm4.PNG","/assets/2020-Tickets/02.18.20.am7.PNG","/assets/2020-Tickets/02.18.20.pm5.PNG",
-            "/assets/2020-Tickets/02.19.20.am8.PNG","/assets/2020-Tickets/02.19.20.pm5.PNG","/assets/2020-Tickets/02.20.20.am7.PNG","/assets/2020-Tickets/02.20.20.pm5.PNG","/assets/2020-Tickets/02.21.20.am10.PNG","/assets/2020-Tickets/02.21.20.pm5.PNG",
-            "/assets/2020-Tickets/02.23.20.pm5.PNG","/assets/2020-Tickets/02.24.20.am7.PNG","/assets/2020-Tickets/02.24.20.pm5.PNG","/assets/2020-Tickets/02.26.20.pm5.PNG","/assets/2020-Tickets/02.27.20.am7.PNG","/assets/2020-Tickets/02.27.20.pm5.PNG",
-            "/assets/2020-Tickets/02.29.20.pm5.PNG","/assets/2020-Tickets/03.01.20.pm12.PNG","/assets/2020-Tickets/03.03.20.am7.PNG","/assets/2020-Tickets/03.03.20.pm5.PNG","/assets/2020-Tickets/03.04.20.am7.PNG","/assets/2020-Tickets/03.04.20.pm5.PNG",
-            "/assets/2020-Tickets/03.09.20.am7.PNG","/assets/2020-Tickets/03.10.20.am7.PNG","/assets/2020-Tickets/03.13.20.am9.PNG","/assets/2020-Tickets/03.13.20.pm6.PNG"
+        "assets/2020-Tickets/01.02.20.am7.PNG","assets/2020-Tickets/01.02.20.pm5.PNG","assets/2020-Tickets/01.03.20.am7.PNG","assets/2020-Tickets/01.03.20.pm4.PNG","assets/2020-Tickets/01.06.20.am7.PNG",
+            "assets/2020-Tickets/01.06.20.pm5.PNG","assets/2020-Tickets/01.08.20.am7.PNG","assets/2020-Tickets/01.08.20.pm5.PNG","assets/2020-Tickets/01.10.20.am7.PNG","assets/2020-Tickets/01.10.20.pm5.PNG","assets/2020-Tickets/01.13.20.am7.PNG",
+            "assets/2020-Tickets/01.13.20.pm5.PNG","assets/2020-Tickets/01.15.20.am7.PNG","assets/2020-Tickets/01.15.20.pm5.PNG","assets/2020-Tickets/01.16.20.am8.PNG","assets/2020-Tickets/01.16.20.pm4.PNG","assets/2020-Tickets/01.21.20.am7.PNG",
+            "assets/2020-Tickets/01.21.20.pm5.PNG","assets/2020-Tickets/01.22.20.am7.PNG","assets/2020-Tickets/01.22.20.pm5.PNG","assets/2020-Tickets/01.28.20.am7.PNG","assets/2020-Tickets/01.28.20.pm5.PNG","assets/2020-Tickets/01.29.20.am7.PNG",
+            "assets/2020-Tickets/01.29.20.pm5.PNG","assets/2020-Tickets/01.30.20.am7.PNG","assets/2020-Tickets/01.30.20.pm5.PNG","assets/2020-Tickets/02.01.20.am7.PNG","assets/2020-Tickets/02.03.20.pm5.PNG","assets/2020-Tickets/02.04.20.am7.PNG",
+            "assets/2020-Tickets/02.05.20.am7.PNG","assets/2020-Tickets/02.05.20.pm8.PNG","assets/2020-Tickets/02.06.20.am7.PNG","assets/2020-Tickets/02.06.20.pm5.PNG","assets/2020-Tickets/02.10.20.am7.PNG","assets/2020-Tickets/02.10.20.pm5.PNG",
+            "assets/2020-Tickets/02.11.20.am7.PNG","assets/2020-Tickets/02.11.20.pm5.PNG","assets/2020-Tickets/02.13.20.am7.PNG","assets/2020-Tickets/02.13.20.pm4.PNG","assets/2020-Tickets/02.18.20.am7.PNG","assets/2020-Tickets/02.18.20.pm5.PNG",
+            "assets/2020-Tickets/02.19.20.am8.PNG","assets/2020-Tickets/02.19.20.pm5.PNG","assets/2020-Tickets/02.20.20.am7.PNG","assets/2020-Tickets/02.20.20.pm5.PNG","assets/2020-Tickets/02.21.20.am10.PNG","assets/2020-Tickets/02.21.20.pm5.PNG",
+            "assets/2020-Tickets/02.23.20.pm5.PNG","assets/2020-Tickets/02.24.20.am7.PNG","assets/2020-Tickets/02.24.20.pm5.PNG","assets/2020-Tickets/02.26.20.pm5.PNG","assets/2020-Tickets/02.27.20.am7.PNG","assets/2020-Tickets/02.27.20.pm5.PNG",
+            "assets/2020-Tickets/02.29.20.pm5.PNG","assets/2020-Tickets/03.01.20.pm12.PNG","assets/2020-Tickets/03.03.20.am7.PNG","assets/2020-Tickets/03.03.20.pm5.PNG","assets/2020-Tickets/03.04.20.am7.PNG","assets/2020-Tickets/03.04.20.pm5.PNG",
+            "assets/2020-Tickets/03.09.20.am7.PNG","assets/2020-Tickets/03.10.20.am7.PNG","assets/2020-Tickets/03.13.20.am9.PNG","assets/2020-Tickets/03.13.20.pm6.PNG"
         ],
     
-    "2021": [ "/assets/2021-Tickets/12.29.21.pm7.PNG"
+    "2021": [ "assets/2021-Tickets/12.29.21.pm7.PNG"
     ], 
 //2022 image file paths
-    "2022": [ "/assets/2022-Tickets/02.15.22.pm1.PNG","/assets/2022-Tickets/02.15.22.pm2.PNG","/assets/2022-Tickets/05.09.22.am7.PNG","/assets/2022-Tickets/05.19.22.am7.PNG",
-        "/assets/2022-Tickets/06.16.22.pm1.PNG","/assets/2022-Tickets/06.18.22.am9.PNG","/assets/2022-Tickets/06.18.22.pm4.PNG","/assets/2022-Tickets/06.27.22.am9.PNG","/assets/2022-Tickets/06.27.22.pm2.PNG",
-        "/assets/2022-Tickets/07.20.22.am7.PNG","/assets/2022-Tickets/07.20.22.pm5.PNG","/assets/2022-Tickets/08.02.22.am6.PNG",
-        "/assets/2022-Tickets/09.06.22.am7.PNG","/assets/2022-Tickets/09.07.22.am7.PNG",
-        "/assets/2022-Tickets/09.08.22.am7.PNG","/assets/2022-Tickets/09.13.22.am8.PNG","/assets/2022-Tickets/09.13.22.pm5.PNG","/assets/2022-Tickets/09.14.22.pm5.PNG","/assets/2022-Tickets/09.15.22.am6.PNG","/assets/2022-Tickets/09.15.22.pm5.PNG",
-        "/assets/2022-Tickets/09.20.22.am7.PNG","/assets/2022-Tickets/09.20.22.pm5.PNG","/assets/2022-Tickets/09.22.22.am6.PNG","/assets/2022-Tickets/09.22.22.pm5.PNG","/assets/2022-Tickets/09.27.22.am8.PNG","/assets/2022-Tickets/09.27.22.pm4.PNG",
-        "/assets/2022-Tickets/10.11.22.pm5.PNG","/assets/2022-Tickets/10.13.22.am7.PNG","/assets/2022-Tickets/10.13.22.pm5.PNG","/assets/2022-Tickets/10.18.22.pm4.PNG","/assets/2022-Tickets/10.20.22.am7.PNG","/assets/2022-Tickets/10.20.22.pm4.PNG",
-        "/assets/2022-Tickets/11.01.22.pm6.PNG","/assets/2022-Tickets/11.14.22.am7.PNG","/assets/2022-Tickets/11.14.22.pm5.PNG","/assets/2022-Tickets/11.15.22.pm3.PNG","/assets/2022-Tickets/11.15.22.pm4.PNG","/assets/2022-Tickets/11.21.22.am8.PNG",
-        "/assets/2022-Tickets/11.26.22.pm10.PNG","/assets/2022-Tickets/11.28.22.am7.PNG","/assets/2022-Tickets/11.28.22.pm5.PNG","/assets/2022-Tickets/11.29.22.am7.PNG","/assets/2022-Tickets/12.20.22.am8.PNG","/assets/2022-Tickets/12.21.22.am7.PNG",
+    "2022": [ "assets/2022-Tickets/02.15.22.pm1.PNG","assets/2022-Tickets/02.15.22.pm2.PNG","assets/2022-Tickets/05.09.22.am7.PNG","assets/2022-Tickets/05.19.22.am7.PNG",
+        "assets/2022-Tickets/06.16.22.pm1.PNG","assets/2022-Tickets/06.18.22.am9.PNG","assets/2022-Tickets/06.18.22.pm4.PNG","assets/2022-Tickets/06.27.22.am9.PNG","assets/2022-Tickets/06.27.22.pm2.PNG",
+        "assets/2022-Tickets/07.20.22.am7.PNG","assets/2022-Tickets/07.20.22.pm5.PNG","assets/2022-Tickets/08.02.22.am6.PNG",
+        "assets/2022-Tickets/09.06.22.am7.PNG","assets/2022-Tickets/09.07.22.am7.PNG",
+        "assets/2022-Tickets/09.08.22.am7.PNG","assets/2022-Tickets/09.13.22.am8.PNG","assets/2022-Tickets/09.13.22.pm5.PNG","assets/2022-Tickets/09.14.22.pm5.PNG","assets/2022-Tickets/09.15.22.am6.PNG","assets/2022-Tickets/09.15.22.pm5.PNG",
+        "assets/2022-Tickets/09.20.22.am7.PNG","assets/2022-Tickets/09.20.22.pm5.PNG","assets/2022-Tickets/09.22.22.am6.PNG","assets/2022-Tickets/09.22.22.pm5.PNG","assets/2022-Tickets/09.27.22.am8.PNG","assets/2022-Tickets/09.27.22.pm4.PNG",
+        "assets/2022-Tickets/10.11.22.pm5.PNG","assets/2022-Tickets/10.13.22.am7.PNG","assets/2022-Tickets/10.13.22.pm5.PNG","assets/2022-Tickets/10.18.22.pm4.PNG","assets/2022-Tickets/10.20.22.am7.PNG","assets/2022-Tickets/10.20.22.pm4.PNG",
+        "assets/2022-Tickets/11.01.22.pm6.PNG","assets/2022-Tickets/11.14.22.am7.PNG","assets/2022-Tickets/11.14.22.pm5.PNG","assets/2022-Tickets/11.15.22.pm3.PNG","assets/2022-Tickets/11.15.22.pm4.PNG","assets/2022-Tickets/11.21.22.am8.PNG",
+        "assets/2022-Tickets/11.26.22.pm10.PNG","assets/2022-Tickets/11.28.22.am7.PNG","assets/2022-Tickets/11.28.22.pm5.PNG","assets/2022-Tickets/11.29.22.am7.PNG","assets/2022-Tickets/12.20.22.am8.PNG","assets/2022-Tickets/12.21.22.am7.PNG",
 
     ],
 
     "2023": [
-        "/assets/2023-Tickets/01.03.23.am11.PNG","/assets/2023-Tickets/01.05.23.am7.PNG","/assets/2023-Tickets/01.05.23.pm5.PNG","/assets/2023-Tickets/01.09.23.am7.PNG",
-        "/assets/2023-Tickets/01.09.23.pm5.PNG","/assets/2023-Tickets/01.10.23.am7.PNG","/assets/2023-Tickets/01.10.23.pm5.PNG","/assets/2023-Tickets/01.17.23.pm3.PNG",
-        "/assets/2023-Tickets/01.24.23.am8.PNG","/assets/2023-Tickets/01.24.23.pm5.PNG","/assets/2023-Tickets/01.25.23.pm4.PNG","/assets/2023-Tickets/02.01.23.am8.PNG",
-        "/assets/2023-Tickets/02.01.23.pm5.PNG","/assets/2023-Tickets/02.03.23.am8.PNG","/assets/2023-Tickets/02.15.23.am7.PNG","/assets/2023-Tickets/06.15.23.pm4.PNG",
-        "/assets/2023-Tickets/06.20.23.pm4.PNG","/assets/2023-Tickets/06.21.23.am7.PNG","/assets/2023-Tickets/06.22.23.pm10.PNG","/assets/2023-Tickets/06.25.23.pm5.PNG",
-        "/assets/2023-Tickets/07.06.23.pm5.PNG","/assets/2023-Tickets/07.13.23.am8.PNG","/assets/2023-Tickets/07.20.23.am8.PNG","/assets/2023-Tickets/08.31.23.pm5.PNG",
-        "/assets/2023-Tickets/09.18.23.am8.PNG","/assets/2023-Tickets/09.22.23.pm5.PNG","/assets/2023-Tickets/09.26.23.pm5.PNG","/assets/2023-Tickets/10.03.23.pm5.PNG",
+        "assets/2023-Tickets/01.03.23.am11.PNG","assets/2023-Tickets/01.05.23.am7.PNG","assets/2023-Tickets/01.05.23.pm5.PNG","assets/2023-Tickets/01.09.23.am7.PNG",
+        "assets/2023-Tickets/01.09.23.pm5.PNG","assets/2023-Tickets/01.10.23.am7.PNG","assets/2023-Tickets/01.10.23.pm5.PNG","assets/2023-Tickets/01.17.23.pm3.PNG",
+        "assets/2023-Tickets/01.24.23.am8.PNG","assets/2023-Tickets/01.24.23.pm5.PNG","assets/2023-Tickets/01.25.23.pm4.PNG","assets/2023-Tickets/02.01.23.am8.PNG",
+        "assets/2023-Tickets/02.01.23.pm5.PNG","assets/2023-Tickets/02.03.23.am8.PNG","assets/2023-Tickets/02.15.23.am7.PNG","assets/2023-Tickets/06.15.23.pm4.PNG",
+        "assets/2023-Tickets/06.20.23.pm4.PNG","assets/2023-Tickets/06.21.23.am7.PNG","assets/2023-Tickets/06.22.23.pm10.PNG","assets/2023-Tickets/06.25.23.pm5.PNG",
+        "assets/2023-Tickets/07.06.23.pm5.PNG","assets/2023-Tickets/07.13.23.am8.PNG","assets/2023-Tickets/07.20.23.am8.PNG","assets/2023-Tickets/08.31.23.pm5.PNG",
+        "assets/2023-Tickets/09.18.23.am8.PNG","assets/2023-Tickets/09.22.23.pm5.PNG","assets/2023-Tickets/09.26.23.pm5.PNG","assets/2023-Tickets/10.03.23.pm5.PNG",
     ], 
 
     "2024": [
-        "/assets/2024-Tickets/01.08.24.pm5.PNG",
-        "/assets/2024-Tickets/05.10.24.pm4.PNG",
-        "/assets/2024-Tickets/05.14.24.am8.PNG",
-        "/assets/2024-Tickets/05.21.24.pm5.PNG",
-        "/assets/2024-Tickets/05.23.24.am8.PNG",
-        "/assets/2024-Tickets/05.28.24.am7.PNG",
-        "/assets/2024-Tickets/05.23.24.am11.PNG"
+        "assets/2024-Tickets/01.08.24.pm5.PNG",
+        "assets/2024-Tickets/05.10.24.pm4.PNG",
+        "assets/2024-Tickets/05.14.24.am8.PNG",
+        "assets/2024-Tickets/05.21.24.pm5.PNG",
+        "assets/2024-Tickets/05.23.24.am8.PNG",
+        "assets/2024-Tickets/05.28.24.am7.PNG",
+        "assets/2024-Tickets/05.23.24.am11.PNG"
     ], 
  };
 
